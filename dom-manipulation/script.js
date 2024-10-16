9d4369432fa963425f6a94959cafc095167cbe4e
Nasr <nasr.asri10@gmail.com>
2024-10-16 13:53:21 +0100

    fix quotesArray

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,17 +4,20 @@ const quoteDisplay = document.getElementById('quoteDisplay');
 newQuoteBtn.addEventListener('click', () => {
     const para = document.createElement('p');
     quoteDisplay.appendChild(para);
+
+    const quotesArray = [
+        {text: "text", category: "category"},
+        {text: "text", category: "category"},
+        {text: "text", category: "category"},
+        {text: "text", category: "category"}
+      ];
+    
+      
+    function showRandomQuote() {
+        const randomIndex = Math.floor(Math.random() * quotesArray.length);
+        const randomQuote = quotesArray[randomIndex];
+    }
 })
-
-const quotesArray = [
-    {"text", "category"}
-  ];
-
-  
-function showRandomQuote() {
-    const randomIndex = Math.floor(Math.random() * quotesArray.length);
-    const randomQuote = quotesArray[randomIndex];
-}
 
 function createAddQuoteForm() {
     const form = document.createElement('form');
