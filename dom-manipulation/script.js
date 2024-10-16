8da075e7d08a1c6c7d5b5cf8f0867ef4f5891256
Nasr <nasr.asri10@gmail.com>
2024-10-16 14:18:47 +0100

    fix elements positioning

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,23 +1,20 @@
 const newQuoteBtn = document.getElementById('newQuote');
 const quoteDisplay = document.getElementById('quoteDisplay');
 
-newQuoteBtn.addEventListener('click', () => {
-   
+const quotesArray = [
+    {text: "text", category: "category"},
+    {text: "text", category: "category"},
+    {text: "text", category: "category"},
+    {text: "text", category: "category"}
+  ];
 
-    const quotesArray = [
-        {text: "text", category: "category"},
-        {text: "text", category: "category"},
-        {text: "text", category: "category"},
-        {text: "text", category: "category"}
-      ];
-    
-      
+newQuoteBtn.addEventListener('click', () => {
     function showRandomQuote() {
-        const para = document.createElement('p');
-        quoteDisplay.appendChild(para);
-        const randomIndex = Math.floor(Math.random() * quotesArray.length);
-        const randomQuote = quotesArray[randomIndex];
-        para.innerHTML = `Quote: ${randomQuote.text} Category: ${randomQuote.category}`;
+    const para = document.createElement('p');
+    quoteDisplay.appendChild(para);
+    const randomIndex = Math.floor(Math.random() * quotesArray.length);
+    const randomQuote = quotesArray[randomIndex];
+    para.innerHTML = `Quote: ${randomQuote.text} Category: ${randomQuote.category}`;
     }
 
     function createAddQuoteForm() {
