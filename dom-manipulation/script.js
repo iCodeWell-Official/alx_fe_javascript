88a46b94a4ecfd357b141e8d43f12f9aa4742848
Nasr <nasr.asri10@gmail.com>
2024-10-16 14:16:33 +0100

    fix positioning of elements

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -2,8 +2,7 @@ const newQuoteBtn = document.getElementById('newQuote');
 const quoteDisplay = document.getElementById('quoteDisplay');
 
 newQuoteBtn.addEventListener('click', () => {
-    const para = document.createElement('p');
-    quoteDisplay.appendChild(para);
+   
 
     const quotesArray = [
         {text: "text", category: "category"},
@@ -14,16 +13,19 @@ newQuoteBtn.addEventListener('click', () => {
     
       
     function showRandomQuote() {
+        const para = document.createElement('p');
+        quoteDisplay.appendChild(para);
         const randomIndex = Math.floor(Math.random() * quotesArray.length);
         const randomQuote = quotesArray[randomIndex];
         para.innerHTML = `Quote: ${randomQuote.text} Category: ${randomQuote.category}`;
     }
 
+    function createAddQuoteForm() {
+        const form = document.createElement('div');
+        quoteDisplay.appendChild(form);
+    }
+
     showRandomQuote();
+    createAddQuoteForm();
+
 })
-
-function createAddQuoteForm() {
-    const form = document.createElement('form');
-
-
-}
