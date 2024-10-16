fe20d8d532c25e77663069af31fb874357120872
Nasr <nasr.asri10@gmail.com>
2024-10-16 13:44:21 +0100

    fix array

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -5,3 +5,19 @@ newQuoteBtn.addEventListener('click', () => {
     const para = document.createElement('p');
     quoteDisplay.appendChild(para);
 })
+
+const quotesArray = [
+    {"text", "category"}
+  ];
+
+  
+function showRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotesArray.length);
+    const randomQuote = quotesArray[randomIndex];
+}
+
+function createAddQuoteForm() {
+    const form = document.createElement('form');
+
+
+}
